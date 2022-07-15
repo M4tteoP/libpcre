96466644c5843047f4e7bc779f9cd95e84bc7ad7
M4tteoP <pace.matteo96@gmail.com>
2022-07-15 12:59:46 +0200

    chore: adding some docs inside the readme, minor script refactor

--- build_for_wasm.sh
+++ build_for_wasm.sh
@@ -2,11 +2,6 @@
 
 set -euo pipefail
 
-#export CC="$WASI_SDK_PATH/bin/clang --sysroot=$WASI_SDK_PATH/share/wasi-sysroot"
-#export AR="$WASI_SDK_PATH/bin/llvm-ar"
-#export RANLIB="$WASI_SDK_PATH/bin/llvm-ranlib"
-#export LD="$WASI_SDK_PATH/bin/wasm-ld"
-
 target=$1
 
 if [ -z "$target" ]; then
@@ -20,6 +15,13 @@ MAKE="make"
 if [ "$target" == "wasm32-emscripten" ]; then
   CONFIGURE="emconfigure $CONFIGURE"
   MAKE="emmake $MAKE"
+fi
+
+if [ "$target" == "wasm32-wasi" ]; then
+  export CC="$WASI_SDK_PATH/bin/clang --sysroot=$WASI_SDK_PATH/share/wasi-sysroot"
+  export AR="$WASI_SDK_PATH/bin/llvm-ar"
+  export RANLIB="$WASI_SDK_PATH/bin/llvm-ranlib"
+  export LD="$WASI_SDK_PATH/bin/wasm-ld"
 fi
 
 $CONFIGURE --host wasm32 \
